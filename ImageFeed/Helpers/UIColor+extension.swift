55093bd5f1948ac088a556f5e070fad01c71a651
Pussmal <>
2022-12-19 17:05:15 +0300

    реализован LaunchScreen, добавлены цвета

---
+++ ImageFeed/Helpers/UIColor+extension.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor? { UIColor(named: "ypBackground") }
+    static var ypBlack: UIColor? { UIColor(named: "ypBlack") }
+    static var ypGray: UIColor? { UIColor(named: "ypGray") }
+    static var ypWhite: UIColor? { UIColor(named: "ypWhite") }
+    static var ypWhiteAlpha50: UIColor? { UIColor(named: "ypWhiteAlpha50") }
+    static var ypRed: UIColor? { UIColor(named: "ypRed") }
+    static var ypBlue: UIColor? { UIColor(named: "ypBlue") }
+}
