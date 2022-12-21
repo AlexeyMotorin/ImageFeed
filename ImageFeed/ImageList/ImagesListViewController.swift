86d067ec72ec044838775220dae3ac485393c3f5
Pussmal <aleksey.motorin2015@yandex.ru>
2022-12-21 22:10:42 +0300

    Добавлены картинки для ленты, определен тип для отображения даты, реализован метод настройки ячейки

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -11,6 +11,16 @@ final class ImagesListViewController: UIViewController {
 
     // MARK: - @IBOutlet
     @IBOutlet private var tableView: UITableView!
+    
+    // MARK: - Private properties
+    private let photosName = Array(0..<20).map { "\($0)" }
+    
+    private lazy var dateForrmater: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
    
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -20,14 +30,25 @@ final class ImagesListViewController: UIViewController {
         tableView.delegate = self
     }
     
-    private func config(cell: ImagesListCell) {}
+    private func config(cell: ImagesListCell, for indexPath: IndexPath) {
+       
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+        cell.cellImageView.image = image
+        cell.dateLabel.text = dateForrmater.string(from: Date())
+        
+        print("\(indexPath.row % 2)")
+        
+        let isLikedImage: UIImage? = indexPath.row % 2 == 0 ? UIImage(named: "NoLike") : UIImage(named: "IsLike")
+        cell.likeButton.setImage(isLikedImage, for: .normal)
+    
+    }
     
 }
 
 // MARK: - UITableViewDataSource
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        photosName.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,7 +57,7 @@ extension ImagesListViewController: UITableViewDataSource {
         
         guard let imagesListCell = cell as? ImagesListCell else { return UITableViewCell() }
 
-        config(cell: imagesListCell)
+        config(cell: imagesListCell, for: indexPath)
         return imagesListCell
     }
 }
