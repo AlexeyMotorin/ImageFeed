d053997ad1777668603650b5b144d7f03b1dbe2f
Pussmal <aleksey.motorin2015@yandex.ru>
2023-02-07 15:54:33 +0300

    Исключили риск возникновения гонки

--- ImageFeed/Authorize/Service/OAuth2Service.swift
+++ ImageFeed/Authorize/Service/OAuth2Service.swift
@@ -9,6 +9,11 @@ final class OAuth2Service {
     // MARK: - Private properties
     private let urlSession = URLSession.shared
     
+    //указатель на активную задачу, если задач нет, значение = nil. Значение присваивается до task.resume(), при успешном выполнении обнуляется
+    private var task: URLSessionTask?
+    // значение code которое передается в последнем созданном запросе
+    private var lastCode: String?
+    
     private (set) var authToken: String? {
         get { OAuth2TokenStorage().token }
         set { OAuth2TokenStorage().token = newValue }
@@ -16,20 +21,33 @@ final class OAuth2Service {
     
     // MARK: - Public methods
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void ) {
+        // проверяем что код выполняетс из главного потока, так как при обращении к task и lastCode не из главного потока случится гонка
+        assert(Thread.isMainThread)
+        
+        if lastCode == code { return } // если true, то значит по этому коду мы ранее получили bearer token и сохранили его, делать новый запрос не нужно
+        task?.cancel() // если значение не nil, тогда мы останавливаем задачу, присваиваем новый code в lastCode и запускаем новую задачу
+        lastCode = code
+        
         let request = authTokenRequest(code: code)
         
         let task = object(for: request) { [weak self] result in
             guard let self = self else { return }
-            switch result {
-            case .success(let body):
-                let authToken = body.accessToken
-                self.authToken = authToken
-                completion(.success(authToken))
-            case .failure(let error):
-                completion(.failure(error))
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let body):
+                    let authToken = body.accessToken
+                    self.authToken = authToken
+                    completion(.success(authToken))
+                    self.task = nil // после выполнения задачи обнуляем активную задачу
+                case .failure(let error):
+                    self.lastCode = nil // если пришла ошибка удалим lastCode чтобы повторить запрос с темже кодом
+                    completion(.failure(error))
+                }
             }
         }
-        task.resume()
+        
+        self.task = task // так как задача выполняется асинхронно на главном потоке после ее запуска мы присваиваем ее значение в указатель для активной задачи, после успешного выполнения таска, указатель станет nil
+        task.resume() // запускаем задачу
     }
 }
 
